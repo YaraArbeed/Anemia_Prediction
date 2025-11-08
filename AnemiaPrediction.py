219b447310a0cb0659e5787e293da6793eab21cd
Yara <yaraxd12@gmail.com>
2025-11-09 00:17:54 +0300

    add CORS

--- AnemiaPrediction.py
+++ AnemiaPrediction.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware   
 import joblib
 import numpy as np
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173"   # your local frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # allow POST, GET, OPTIONS, etc.
+    allow_headers=["*"],
+)
 # Load your saved Random Forest model
 model = joblib.load('anemia_rf_model.pkl')
 
